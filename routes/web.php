81bed77cf7826c6cbde1d8cc4427537f5c20e7cd
darkbazory <sonia.l.ortega@gmail.com>
2022-01-14 09:52:59 +0100

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use App\Http\Controllers\CategoriaController;
-=======
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\EventoController;
->>>>>>> 17a8198a01eefc2cdf156feb4c4c51f64226f880
 
 /*
 |--------------------------------------------------------------------------
@@ -29,18 +26,15 @@ Route::get('/agenda', function () {
     return view('agenda');
 });
 
-<<<<<<< HEAD
 Route::resources([
     'categorias' => CategoriaController::class
     // 'posts' => PostController::class,
 ]);
-=======
 Route::resource('eventos',EventoController::class);
 
 Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
->>>>>>> 17a8198a01eefc2cdf156feb4c4c51f64226f880
 
 Auth::routes();
 
