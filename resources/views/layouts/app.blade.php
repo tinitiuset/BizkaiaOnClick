3ab26969a7ffb857c1ef9f30f4cd4a40c70d9452
darkbazory <73275943+darkbazory@users.noreply.github.com>
2022-01-17 12:46:05 +0100

    footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-100"> 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -20,8 +20,8 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/estilos.css') }}" rel="stylesheet">
 </head>
-<body>
-    <div id="app">
+<body class="h-100">
+    <div id="app" class="h-100">
         <nav class="navbar navbar-color navbar-expand-md fa-lg">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
@@ -89,6 +89,23 @@
         <main class="py-4">
             @yield('content')
         </main>
+        <footer class="footer mt-auto navbar-color">
+            <div class="container">
+              <!-- Copyright -->
+              <div class="row pt-2 text-center text-white">
+                  <div class="col">
+                    © 2022 Todos los derechos reservados. <a class="text-white text-decoration-none" href="https://mdbootstrap.com/">BizkaiaOnClick.com</a>
+                  </div>
+              </div>
+              <div class="row pb-2 text-center text-light">
+                  <div class="col">
+                    Politica de privacidad | Términos de uso | Legalidad | <a class="text-white text-decoration-none" href="https://mdbootstrap.com/">Volver arriba</a>
+                  </div>
+              </div>
+              <!-- Copyright -->
+            </div>
+    
+        </footer>
     </div>
 </body>
 </html>
