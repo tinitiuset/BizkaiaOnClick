7b772287bb26d16e1371f653db6725880bfead34
darkbazory <sonia.l.ortega@gmail.com>
2022-01-19 20:05:10 +0100

    activar factory e iconos bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,7 +45,7 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->nombre }} {{ Auth::user()->apellidos }}
+                                    {{ Auth::user()->usuario }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-left" aria-labelledby="navbarDropdown">
