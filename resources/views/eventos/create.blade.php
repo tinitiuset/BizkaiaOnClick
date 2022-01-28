23bf52e55fef8f440d718ca0e9070963db09620f
darkbazory <73275943+darkbazory@users.noreply.github.com>
2022-01-28 10:06:26 +0100

    crud eventos

--- resources/views/eventos/create.blade.php
+++ resources/views/eventos/create.blade.php
@@ -1,1 +1,23 @@
 Formulario de creación de eventos
+<form action="" method="post" enctype="multipart/form-data">
+<input type="text" name="titulo">
+<input type="text" name="descripcion">
+<input type="date" name="fechaInicio">
+<input type="date" name="fechaFin">
+<input type="time" name="hora">
+<input type="float" name="precio">
+<input type="text" name="direccion">
+<input type="enum" name="estado">
+<input type="text" name="sala">
+<input type="text" name="recinto">
+<input type="text" name="localidad">
+<input type="number" name="usuarioAprobador">
+<input type="number" name="usuarioCreador">
+<select name="categoria">
+    @foreach ($categorias as $categoria)
+        <option value="{{$categoria->nombre}}">{{$categoria->nombre}}</option>
+    @endforeach
+    
+</select>
+<input type="submit" name="enviar">
+</form>
